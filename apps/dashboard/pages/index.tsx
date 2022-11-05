adf21ad4e0f9379a00b0ad16d4cb05af5ee13188
BossDaily <gboys2008@gmail.com>
2022-11-05 12:29:25 -0500

    basic sign in

--- apps/dashboard/pages/index.tsx
+++ apps/dashboard/pages/index.tsx
@@ -1,15 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { GetServerSideProps, NextPage } from 'next';
-import React from 'react';
-import { useSession, signIn, signOut } from "next-auth/react"
+import Head from "next/head";
+import Image from "next/image";
+import { GetServerSideProps, NextPage } from "next";
+import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 
-const index:NextPage = () => {
+const index: NextPage = () => {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
-    <div>
-      Enter
-    </div>
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
   );
-}
+};
 
 export default index;
