c69b4c9663b08102b5a75322fc2e605ad7b35541
BossDaily <gboys2008@gmail.com>
2022-11-06 15:49:25 -0600

    d

--- apps/dashboard/pages/api/auth/[...nextauth].ts
+++ apps/dashboard/pages/api/auth/[...nextauth].ts
@@ -1,18 +1,38 @@
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-export const authOptions = {
+export default NextAuth({
   session: {
+    //@ts-ignore
     jwt: true,
   },
   callbacks: {
-    jwt: async (token, user, account, profile) => {
+    //@ts-ignore
+    jwt: async ({ token, user, account, profile }) => {
       if (user) {
         token.profile = profile;
-        token.accessToken = account.accessToken;
-        token.refreshToken = account.refreshToken;
+        token.accessToken = account?.accessToken;
+        token.refreshToken = account?.refreshToken;
       }
-      return Promise.resolve(token);;
+      return Promise.resolve(token);
+    },
+    // @ts-ignore
+    session: async ({ session, token }) => {
+      // @ts-ignore
+      session.accessToken = token.accessToken;
+      // @ts-ignore
+      session.refreshToken = token.refreshToken;
+      // @ts-ignore
+      session.user = token.profile;
+      const res = await fetch(`https://discord.com/api/v10/users/@me/guilds`, {
+        headers: {
+          Authorization: `Bearer ${token.accessToken}`,
+        },
+      });
+      const data = await res.json();
+      // @ts-ignore
+      session.user.profile.guilds = data;
+      return session;
     },
   },
   // Configure one or more authentication providers
@@ -22,8 +42,17 @@ export const authOptions = {
       clientId: process.env.DISCORD_CLIENT_ID,
       // @ts-ignore
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: "identify guilds email connections",
+        },
+      },
+      profile(profile) {
+        return profile;
+      },
+      // ...add more providers here
     }),
-    // ...add more providers here
   ],
-};
-export default NextAuth(authOptions);
+});
+
+// export default NextAuth(authOptions)
