b67b36520fef1f8050ce10774430f949ffaeda3f
gboys2008@gmail.com <gboys2008@gmail.com>
2022-11-07 16:34:38 +0000

    fixed query

--- apps/dashboard/pages/api/auth/[...nextauth].ts
+++ apps/dashboard/pages/api/auth/[...nextauth].ts
@@ -2,39 +2,6 @@ import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
 export default NextAuth({
-  session: {
-    //@ts-ignore
-    jwt: true,
-  },
-  callbacks: {
-    //@ts-ignore
-    jwt: async ({ token, user, account, profile }) => {
-      if (user) {
-        token.profile = profile;
-        token.accessToken = account?.accessToken;
-        token.refreshToken = account?.refreshToken;
-      }
-      return Promise.resolve(token);
-    },
-    // @ts-ignore
-    session: async ({ session, token }) => {
-      // @ts-ignore
-      session.accessToken = token.accessToken;
-      // @ts-ignore
-      session.refreshToken = token.refreshToken;
-      // @ts-ignore
-      session.user = token.profile;
-      const res = await fetch(`https://discord.com/api/v10/users/@me/guilds`, {
-        headers: {
-          Authorization: `Bearer ${token.accessToken}`,
-        },
-      });
-      const data = await res.json();
-      // @ts-ignore
-      session.user.profile.guilds = data;
-      return session;
-    },
-  },
   // Configure one or more authentication providers
   providers: [
     DiscordProvider({
@@ -53,6 +20,33 @@ export default NextAuth({
       // ...add more providers here
     }),
   ],
+  callbacks: {
+    //@ts-ignore
+    jwt: async ({ token, account, profile }) => {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.tokenType = account.token_type;
+      }
+      if (profile) {
+        token.profile = profile;
+      }
+      return token;
+    },
+    // @ts-ignore
+    session: async ({ session, token }) => {
+      // @ts-ignore
+      session.accessToken = token.accessToken;
+      // @ts-ignore
+      session.refreshToken = token.refreshToken;
+      // @ts-ignore
+      session.tokenType = token.tokenType
+      // @ts-ignore
+      session.discordUser = token.profile;
+      // @ts-ignore
+      
+      return session;
+    },
+  },
 });
 
 // export default NextAuth(authOptions)
