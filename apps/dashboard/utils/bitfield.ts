9df1babdd5cc5504dbcd24f033fdf0d78e33e59a
gboys2008@gmail.com <gboys2008@gmail.com>
2022-11-08 16:41:14 +0000

    bitfield checking file (from npm packages)

---
+++ apps/dashboard/utils/bitfield.ts
@@ -0,0 +1,60 @@
+const permissions = {
+  CREATE_INSTANT_INVITE: 0x1,
+  KICK_MEMBERS: 0x2,
+  BAN_MEMBERS: 0x4,
+  ADMINISTRATOR: 0x8,
+  MANAGE_CHANNELS: 0x10,
+  MANAGE_GUILD: 0x20,
+  ADD_REACTIONS: 0x40,
+  VIEW_AUDIT_LOG: 0x80,
+  PRIORITY_SPEAKER: 0x100,
+  STREAM: 0x200,
+  VIEW_CHANNEL: 0x400,
+  SEND_MESSAGES: 0x800,
+  SEND_TTS_MESSAGES: 0x1000,
+  MANAGE_MESSAGES: 0x2000,
+  EMBED_LINKS: 0x4000,
+  ATTACH_FILES: 0x8000,
+  READ_MESSAGE_HISTORY: 0x10000,
+  MENTION_EVERYONE: 0x20000,
+  USE_EXTERNAL_EMOJIS: 0x40000,
+  VIEW_GUILD_INSIGHTS: 0x80000,
+  CONNECT: 0x100000,
+  SPEAK: 0x200000,
+  MUTE_MEMBERS: 0x400000,
+  DEAFEN_MEMBERS: 0x800000,
+  MOVE_MEMBERS: 0x1000000,
+  USE_VAD: 0x2000000,
+  CHANGE_NICKNAME: 0x4000000,
+  MANAGE_NICKNAMES: 0x8000000,
+  MANAGE_ROLES: 0x10000000,
+  MANAGE_WEBHOOKS: 0x20000000,
+  MANAGE_EMOJIS_AND_STICKERS: 0x40000000,
+  USE_APPLICATION_COMMANDS: 0x800000000,
+  REQUEST_TO_SPEAK: 0x100000000,
+  MANAGE_THREADS: 0x400000000,
+  USE_PUBLIC_THREADS: 0x800000000,
+  USE_PRIVATE_THREADS: 0x1000000000,
+  USE_EXTERNAL_STICKERS: 0x2000000000
+};
+module.exports.help = "see http://invaliduser.uk.to/discord-bitfield-calculator"
+module.exports.permissions = (permBitfield) => {
+  let currentPermissions = [];
+  const permissionUpper = Math.floor(permBitfield / 0x100000000);
+  const permissionLower = Math.floor(permBitfield % 0x100000000);
+  for (let key in permissions) {
+      if ((permissions[key] >= 0x100000000 && (permissionUpper & Math.floor(permissions[key] / 0x100000000))) || (permissions[key] < 0x100000000 && (permissionLower & permissions[key]))) {
+          currentPermissions.push(key);
+      } else {
+          continue;
+      };
+  };
+  return currentPermissions;
+}
+export const bitfield = (bitfield) => {
+  if (permissions[bitfield]) {
+      return permissions[bitfield];
+  } else {
+      return new Error("that is not a valid bitfield");
+  };
+}
