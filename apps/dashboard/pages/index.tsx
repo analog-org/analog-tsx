00033a055d5015d6ad2ee38dfaf5f07efa56c721
gboys2008@gmail.com <gboys2008@gmail.com>
2022-11-08 16:14:54 +0000

    getServerSideProps to query guilds

--- apps/dashboard/pages/index.tsx
+++ apps/dashboard/pages/index.tsx
@@ -1,18 +1,25 @@
 import Head from "next/head";
 import Image from "next/image";
-import { GetServerSideProps, NextPage } from "next";
+import {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
 import React from "react";
 import { authOptions } from "./api/auth/[...nextauth]";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { unstable_getServerSession } from "next-auth/next";
 
-const index: NextPage = () => {
+const index: NextPage = ({
+  guilds,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const { data: session } = useSession();
   if (session) {
     return (
       <>
         Signed in as {session.user?.name} <br />
         <button onClick={() => signOut()}>Sign out</button>
+        
       </>
     );
   }
@@ -25,20 +32,28 @@ const index: NextPage = () => {
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await unstable_getServerSession(context.req, context.res, authOptions)
-  const guildFetch = await fetch(`https://discord.com/api/v10/users/@me/guilds`, {
-    headers: {
-      Authorization: `Bearer f`,
-    },
-  });;
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
+  const guildFetch = await fetch(
+    `https://discord.com/api/v10/users/@me/guilds`,
+    {
+      headers: {
+        // @ts-ignore
+        Authorization: `Bearer ${session?.accessToken}`,
+      },
+    }
+  );
   const guilds = await guildFetch.json();
-
- 
-
+  
+  console.log(session)
   return {
     props: {
-      guilds
+      guilds,
     },
   };
 };
 
+export default index;
