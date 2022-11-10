9a9411a150d1fd29ce33f3bd206fac86d40aad77
gboys2008@gmail.com <gboys2008@gmail.com>
2022-11-10 16:50:21 +0000

    boilerplate navbar

--- apps/dashboard/pages/index.tsx
+++ apps/dashboard/pages/index.tsx
@@ -9,7 +9,8 @@ import React from "react";
 import { authOptions } from "./api/auth/[...nextauth]";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { unstable_getServerSession } from "next-auth/next";
-import perms from "../utils/bitfield"
+import perms from "../utils/bitfield";
+import { Avatar, Dropdown, Navbar } from "flowbite-react";
 
 const index: NextPage = ({
   guilds,
@@ -18,19 +19,62 @@ const index: NextPage = ({
   if (session) {
     return (
       <>
+        <Navbar fluid={true} rounded={true}>
+          <Navbar.Brand href="https://flowbite.com/">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="mr-3 h-6 sm:h-9"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+              Flowbite
+            </span>
+          </Navbar.Brand>
+          <div className="flex md:order-2">
+            <Dropdown
+              arrowIcon={false}
+              inline={true}
+              label={
+                <Avatar
+                  alt="User settings"
+                  img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                  rounded={true}
+                />
+              }
+            >
+              <Dropdown.Header>
+                <span className="block text-sm">Bonnie Green</span>
+                <span className="block truncate text-sm font-medium">
+                  name@flowbite.com
+                </span>
+              </Dropdown.Header>
+              <Dropdown.Item>Dashboard</Dropdown.Item>
+              <Dropdown.Item>Settings</Dropdown.Item>
+              <Dropdown.Item>Earnings</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item>Sign out</Dropdown.Item>
+            </Dropdown>
+            <Navbar.Toggle />
+          </div>
+          <Navbar.Collapse>
+            <Navbar.Link href="/navbars" active={true}>
+              Home
+            </Navbar.Link>
+            <Navbar.Link href="/navbars">About</Navbar.Link>
+            <Navbar.Link href="/navbars">Services</Navbar.Link>
+            <Navbar.Link href="/navbars">Pricing</Navbar.Link>
+            <Navbar.Link href="/navbars">Contact</Navbar.Link>
+          </Navbar.Collapse>
+        </Navbar>
         Signed in as {session.user?.name} <br />
         <button onClick={() => signOut()}>Sign out</button>
         <p>Guilds you can invite me to: </p>
-        {
-          guilds.map((gld: guild) => {
-            const serverPerms = perms(gld.permissions);
-            if(serverPerms.includes('MANAGE_GUILD')){
-              return (
-                <div>{gld.name}</div>
-              )
-            }
-          })
-        }
+        {guilds.map((gld: guild) => {
+          const serverPerms = perms(gld.permissions);
+          if (serverPerms.includes("MANAGE_GUILD")) {
+            return <div>{gld.name}</div>;
+          }
+        })}
       </>
     );
   }
@@ -58,8 +102,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
   );
   const guilds = await guildFetch.json();
-  
-  console.log(session)
+
+  console.log(session);
   return {
     props: {
       guilds,
