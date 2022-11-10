3d972de9698fd41ca0e0839447a4faea9d140f96
gboys2008@gmail.com <gboys2008@gmail.com>
2022-11-10 16:50:03 +0000

    changed node_modules path

--- apps/dashboard/tailwind.config.js
+++ apps/dashboard/tailwind.config.js
@@ -3,9 +3,10 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
-    './src/**/*.{js,jsx,ts,tsx}'
+    '../../node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    '../../node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/**/*.html'
   ],
   theme: {
     fontFamily: {
