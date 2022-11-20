0297df49937973a77839c107beac7dc19c10abe4
BossDaily <gboys2008@gmail.com>
2022-11-20 15:52:22 -0600

    added githubuserconent for image domain whitelist

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['cdn.discordapp.com', 'discord.com'],
+    domains: ['cdn.discordapp.com', 'discord.com', 'avatars.githubusercontent.com'],
+  },
+  compiler:{
+    styledComponents: true,
   },
 }
 
