2456b204975207c6af64ef5db1362bdbd60cc798
BossDaily <gboys2008@gmail.com>
2022-11-21 11:59:16 -0600

    global navbar component

--- apps/dashboard/src/pages/_app.tsx
+++ apps/dashboard/src/pages/_app.tsx
@@ -3,11 +3,13 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Flowbite } from "flowbite-react";
 import { customtheme as theme } from "../components/theme";
+import Navbar from "../components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Flowbite theme={{ theme }}>
       <SessionProvider session={pageProps.session}>
+        <Navbar />
         <Component {...pageProps} />
       </SessionProvider>
     </Flowbite>
--- apps/dashboard/src/pages/index.tsx
+++ apps/dashboard/src/pages/index.tsx
@@ -19,91 +19,13 @@ const index: NextPage = () => {
   if (session) {
     return (
       <div className="bg-gray-800">
-        <Navbar fluid={true} rounded={true}>
-          <Navbar.Brand href="https://flowbite.com/">
-            <Image
-              src="https://avatars.githubusercontent.com/u/110413696?s=200&v=4"
-              className="mr-5 h-6 sm:h-9 rounded-xl"
-              alt="Flowbite Logo"
-              width={36}
-              height={36}
-            />
-            <span className="ml-3 self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-              Analog
-            </span>
-          </Navbar.Brand>
-          <div className="flex md:order-2">
-            <Dropdown
-              arrowIcon={false}
-              inline={true}
-              label={
-                <Avatar
-                  alt="User settings"
-                  img={session.discordUser.image_url}
-                  rounded={true}
-                />
-              }
-            >
-              <Dropdown.Header>
-                <span className="block text-sm">
-                  {session.discordUser.username}
-                </span>
-                <span className="block truncate text-sm font-medium">
-                  {session.discordUser.email}
-                </span>
-              </Dropdown.Header>
-              <Dropdown.Item onClick={() => signOut()}>Sign out</Dropdown.Item>
-            </Dropdown>
-            <Navbar.Toggle />
-          </div>
-          <Navbar.Collapse>
-            <Navbar.Link href="" active={true}>
-              Home
-            </Navbar.Link>
-            <Navbar.Link href="/dashboard" active={true}>
-              Dashboard
-            </Navbar.Link>
-          </Navbar.Collapse>
-        </Navbar>
+        {"w you're signed in"}
       </div>
     );
   } else {
     return (
       <div>
-        <Navbar fluid={true} rounded={true}>
-          <Navbar.Brand href="/">
-            <Image
-              src="https://avatars.githubusercontent.com/u/110413696?s=200&v=4"
-              className="mr-3 h-6 sm:h-9 rounded-xl"
-              alt="Flowbite Logo"
-              width={36}
-              height={36}
-            />
-            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-              Analog
-            </span>
-          </Navbar.Brand>
-          <div className="flex md:order-2">
-            <Dropdown
-              arrowIcon={false}
-              inline={true}
-              label={
-                <p onClick={() => signIn()} className="text-white">
-                  Sign in
-                </p>
-              }
-            ></Dropdown>
-            <Navbar.Toggle />
-          </div>
-          <Navbar.Collapse>
-            <Navbar.Link href="" active={true}>
-              Home
-            </Navbar.Link>
-            <Navbar.Link href="/dashboard" active={true}>
-              Dashboard
-            </Navbar.Link>
-          </Navbar.Collapse>
-        </Navbar>
+        {"l you're not signed in"}
       </div>
     );
   }
