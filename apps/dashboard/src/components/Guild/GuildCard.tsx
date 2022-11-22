04e6de075f5147dd6756bcaa84db2d34e6662a11
BossDaily <gboys2008@gmail.com>
2022-11-22 11:20:45 -0600

    changed a tag to next link

--- apps/dashboard/src/components/Guild/GuildCard.tsx
+++ apps/dashboard/src/components/Guild/GuildCard.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import { ReactNode } from "react";
 import GuildIcon from "./GuildIcon";
+import Link from "next/link";
 
 type Props = {
   guildId: String;
@@ -11,18 +12,33 @@ type Props = {
 };
 
 const GuildCard: NextPage<Props> = ({ guildIcon, guildId, userDiscriminator, guildName, guildSetup }) => {
-  return (
-    <a
-      href={`https://discord.com/api/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID}&permissions=8&scope=bot%20applications.commands`}
-    >
-      <div className="h-20 flex gap-4 flex-row w-auto bg-black rounded-xl  hover:-translate-y-1 hover:scale-110">
-        <GuildIcon guildIcon={guildIcon} guildId={guildId} userDiscriminator={userDiscriminator}/>
-        <h1 className="text-white font-helvetica font-bold text-3xl pt-4">
-          {guildName}
-        </h1>
-      </div>
-    </a>
-  );
+  if (guildSetup){
+    return (
+      <Link
+        href={`https://discord.com/api/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID}&permissions=8&scope=bot%20applications.commands`}
+      >
+        <div className="h-20 flex gap-4 flex-row w-auto bg-black rounded-xl  hover:-translate-y-1 hover:scale-110 border-green-600">
+          <GuildIcon guildIcon={guildIcon} guildId={guildId} userDiscriminator={userDiscriminator}/>
+          <h1 className="text-white font-helvetica font-bold text-3xl pt-4">
+            {guildName}
+          </h1>
+        </div>
+      </Link>
+    );
+  } else {
+    return (
+      <a
+        href={`https://discord.com/api/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID}&permissions=8&scope=bot%20applications.commands`}
+      >
+        <div className="h-20 flex gap-4 flex-row w-auto bg-black rounded-xl  hover:-translate-y-1 hover:scale-110">
+          <GuildIcon guildIcon={guildIcon} guildId={guildId} userDiscriminator={userDiscriminator}/>
+          <h1 className="text-white font-helvetica font-bold text-3xl pt-4">
+            {guildName}
+          </h1>
+        </div>
+      </a>
+    );
+  }
 };
 
 export default GuildCard;
