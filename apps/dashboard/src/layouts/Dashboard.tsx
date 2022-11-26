0dbc663a5f1530d07369a3274b45db06c9e11f23
BossDaily <gboys2008@gmail.com>
2022-11-26 11:53:05 -0600

    basic untested layout

---
+++ apps/dashboard/src/layouts/Dashboard.tsx
@@ -0,0 +1,23 @@
+import MainFooter from "../components/Footer";
+import NavBar from "../components/Navbar";
+import SideBar from "../components/Sidebar";
+import { ReactNode } from "react";
+
+type LayoutProps = {
+  children: ReactNode;
+}
+
+const DashboardLayout = ({ children }: LayoutProps) => {
+  return (
+    <div className="bg-gray-800">
+      <NavBar />
+      <div className="flex">
+        <SideBar />
+        <div className="flex-grow">{children}</div>
+      </div>
+      <MainFooter />
+    </div>
+  );
+};
+
+export default DashboardLayout;
