5ad2ddb4d71d68016c41a5d97389ab6081875618
gboys2008@gmail.com <gboys2008@gmail.com>
2022-12-06 16:11:10 +0000

    serversideProps

--- apps/dashboard/src/components/EmbedBuilder/Builder.tsx
+++ apps/dashboard/src/components/EmbedBuilder/Builder.tsx
@@ -29,13 +29,18 @@ import {
   DiscordThreadMessage,
   DiscordCustomEmoji,
 } from "@skyra/discord-components-react";
-import { NextPage } from "next";
+import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next";
 import { ReactNode } from "react";
 import Image from "next/image";
 import { Avatar, Dropdown, Navbar, Footer, Accordion } from "flowbite-react";
 import InputContainer from "./InputContainer";
+import { unstable_getServerSession } from "next-auth/next";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
 
-const Builder: NextPage = () => {
+const Builder: NextPage = ({
+  guilds,
+  botProfile,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <div className="py-2 pr-4 flex flex-row gap-2">
       <InputContainer>
@@ -60,4 +65,38 @@ const Builder: NextPage = () => {
   );
 };
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
+  const guildFetch = await fetch(
+    `https://discord.com/api/v10/users/@me/guilds`,
+    {
+      headers: {
+        // @ts-ignore
+        Authorization: `Bearer ${session?.accessToken}`,
+      },
+    }
+  );
+  const guilds = await guildFetch.json();
+  const botProfileFetch = await fetch(
+    `https://discord.com/api/v10/users/@me/guilds`,
+    {
+      headers: {
+        // @ts-ignore
+        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+      },
+    }
+  );
+  const botProfile = await botProfileFetch.json();
+  
+  return {
+    props: {
+      guilds,
+      botProfile,
+    },
+  };
+};
 export default Builder;
