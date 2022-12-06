f5655775cce910c01377288a2b928c3d2b927fb0
BossDaily <gboys2008@gmail.com>
2022-12-05 18:50:33 -0600

    imported input container

--- apps/dashboard/src/components/EmbedBuilder/Builder.tsx
+++ apps/dashboard/src/components/EmbedBuilder/Builder.tsx
@@ -33,38 +33,27 @@ import { NextPage } from "next";
 import { ReactNode } from "react";
 import Image from "next/image";
 import { Avatar, Dropdown, Navbar, Footer, Accordion } from "flowbite-react";
+import InputContainer from "./InputContainer";
 
 const Builder: NextPage = () => {
   return (
     <div className="py-2 pr-4 flex flex-row gap-2">
-      <div>
+      <InputContainer>
         <Accordion alwaysOpen={true}>
           <Accordion.Panel>
-            <Accordion.Title>What is Flowbite?</Accordion.Title>
+            <Accordion.Title>Embed</Accordion.Title>
             <Accordion.Content>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                Flowbite is an open-source library of interactive components
-                built on top of Tailwind CSS including buttons, dropdowns,
-                modals, navbars, and more.
-              </p>
-              <p className="text-gray-500 dark:text-gray-400">
-                Check out this guide to learn how to{" "}
-                <a
-                  href="https://flowbite.com/docs/getting-started/introduction/"
-                  className="text-blue-600 hover:underline dark:text-blue-500"
-                >
-                  get started
-                </a>{" "}
-                and start developing websites even faster with components on top
-                of Tailwind CSS.
-              </p>
+               fdsafsadfsdfa
             </Accordion.Content>
           </Accordion.Panel>
         </Accordion>
-      </div>
+      </InputContainer>
       <div className="rounded-2xl">
         <DiscordMessages>
-          <DiscordMessage author="Analog-tsx" avatar=""> Hello, World! </DiscordMessage>
+          <DiscordMessage author="Analog-tsx">
+            
+            Hello, World!
+          </DiscordMessage>
         </DiscordMessages>
       </div>
     </div>
