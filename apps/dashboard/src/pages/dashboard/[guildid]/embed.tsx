224e854c961f269cec150588d805d1de90e674ab
gboys2008@gmail.com <gboys2008@gmail.com>
2022-12-06 16:40:02 +0000

    added bot profile prop

--- apps/dashboard/src/pages/dashboard/[guildid]/embed.tsx
+++ apps/dashboard/src/pages/dashboard/[guildid]/embed.tsx
@@ -1,11 +1,16 @@
-import { NextPage } from "next";
+import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next";
 import { ReactNode } from "react";
 import { useRouter } from "next/router";
 import type { NextPageWithLayout } from '../../_app'
 import DashboardLayout from "../../../layouts/Dashboard";
 import Builder from "../../../components/EmbedBuilder/Builder";
+import { unstable_getServerSession } from "next-auth/next";
+import { authOptions } from "../../api/auth/[...nextauth]";
 
- const Home: NextPageWithLayout = () => {
+ const Home: NextPageWithLayout = ({
+  guilds,
+  botProfile,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
   const { guildid } = router.query
   console.log(router.asPath);
@@ -14,9 +19,44 @@ import Builder from "../../../components/EmbedBuilder/Builder";
     <div className="text-white">
       
         Guild Id: {guildid}
-        <Builder />
+        <Builder botProfile={botProfile} />
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
+  const guildFetch = await fetch(
+    `https://discord.com/api/v10/users/@me/guilds`,
+    {
+      headers: {
+        // @ts-ignore
+        Authorization: `Bearer ${session?.accessToken}`,
+      },
+    }
+  );
+  const guilds = await guildFetch.json();
+  const botProfileFetch = await fetch(
+    `https://discord.com/api/v10/users/@me`,
+    {
+      headers: {
+        // @ts-ignore
+        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+      },
+    }
+  );
+  const botProfile: user = await botProfileFetch.json();
+  
+  return {
+    props: {
+      guilds,
+      botProfile,
+    },
+  };
 };
 
 export default Home;
