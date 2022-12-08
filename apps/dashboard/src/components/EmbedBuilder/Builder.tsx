54fb0b84673bf9eb2df0e61565413c6b24d5352c
BossDaily <gboys2008@gmail.com>
2022-12-07 19:38:56 -0600

    color picker properly changes state

--- apps/dashboard/src/components/EmbedBuilder/Builder.tsx
+++ apps/dashboard/src/components/EmbedBuilder/Builder.tsx
@@ -138,18 +138,22 @@ const Builder: NextPage<Props> = ({ botProfile }) => {
                     </label>
                     <Dropdown
                       label={
-                        <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                        <input
+                          value={embed.color}
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        />
                       }
                       inline
                     >
                       <Dropdown.Item>
                         <Colorful
-                        disableAlpha={true}
-                        color={embed.color}
-                        onChange={(color) => setEmbed(...embed, color: color.hexa)}
-                      />
+                          disableAlpha={true}
+                          color={embed.color}
+                          onChange={(colors) =>
+                            setEmbed({ ...embed, color: colors.hex })
+                          }
+                        />
                       </Dropdown.Item>
-                      
                     </Dropdown>
                   </div>
                 </div>
@@ -166,7 +170,11 @@ const Builder: NextPage<Props> = ({ botProfile }) => {
             bot={true}
             verified={true}
           >
-            <DiscordEmbed slot="embeds" embedTitle={embed.title}>
+            <DiscordEmbed
+              slot="embeds"
+              embedTitle={embed.title}
+              color={embed.color}
+            >
               <DiscordEmbedDescription slot="description">
                 {embed.description}
               </DiscordEmbedDescription>
