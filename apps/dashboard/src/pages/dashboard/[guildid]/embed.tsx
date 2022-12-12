c2b93b48949380bca0e92619f3e1fce0dab2f0b7
gboys2008@gmail.com <gboys2008@gmail.com>
2022-12-12 16:44:20 +0000

    queried guild roles

--- apps/dashboard/src/pages/dashboard/[guildid]/embed.tsx
+++ apps/dashboard/src/pages/dashboard/[guildid]/embed.tsx
@@ -1,30 +1,34 @@
-import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next";
+import {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  NextPage,
+} from "next";
 import { ReactNode } from "react";
 import { useRouter } from "next/router";
-import type { NextPageWithLayout } from '../../_app'
+import type { NextPageWithLayout } from "../../_app";
 import DashboardLayout from "../../../layouts/Dashboard";
 import Builder from "../../../components/EmbedBuilder/Builder";
 import { unstable_getServerSession } from "next-auth/next";
 import { authOptions } from "../../api/auth/[...nextauth]";
 
- const Home: NextPageWithLayout = ({
+const Home: NextPageWithLayout = ({
   guilds,
   botProfile,
+  roles,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
-  const { guildid } = router.query
+  const { guildid } = router.query;
 
   return (
     <div className="text-white">
-      
-        Guild Id: {guildid}
-        <Builder botProfile={botProfile} />
+      Guild Id: {guildid}
+      <Builder botProfile={botProfile} rolesList={roles} />
     </div>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const guildId = context.query.id
+  const guildId = context.query.id;
 
   const session = await unstable_getServerSession(
     context.req,
@@ -41,41 +45,37 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
   );
   const guilds = await guildFetch.json();
-  const botProfileFetch = await fetch(
-    `https://discord.com/api/v10/users/@me`,
-    {
-      headers: {
-        // @ts-ignore
-        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
-      },
-    }
-  );
+  const botProfileFetch = await fetch(`https://discord.com/api/v10/users/@me`, {
+    headers: {
+      // @ts-ignore
+      Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+    },
+  });
   const botProfile: user = await botProfileFetch.json();
 
   const rolesFetch = await fetch(
-    `https://discord.com/api/v10/users/@me`,
+    `https://discord.com/api/v10/guilds/${guildId}/channels`,
     {
       headers: {
         // @ts-ignore
-        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+        Authorization: `Bearer ${session?.accessToken}`,
       },
     }
   );
-  
+
+  const roles = await rolesFetch.json();
+
   return {
     props: {
       guilds,
       botProfile,
+      roles,
     },
   };
 };
 
 export default Home;
 
-Home.getLayout = function getLayout(page:React.ReactElement) {
-  return (
-    <DashboardLayout>
-      {page}
-    </DashboardLayout>
-  )
-}
+Home.getLayout = function getLayout(page: React.ReactElement) {
+  return <DashboardLayout>{page}</DashboardLayout>;
+};
