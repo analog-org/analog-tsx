ccb230eeeb5e1f72198134ca359c921e22eb9f4d
BossDaily <gboys2008@gmail.com>
2022-12-13 20:22:40 -0600

    added theme

--- apps/dashboard/src/components/Selection/ChannelSelection/ChannelSelection.tsx
+++ apps/dashboard/src/components/Selection/ChannelSelection/ChannelSelection.tsx
@@ -1,20 +1,18 @@
 import { NextPage } from "next";
 import React, { useState } from "react";
-import { type APIChannel } from 'discord-api-types/v10';
+import { type APIChannel } from "discord-api-types/v10";
 
 import Select from "react-select";
 
-
-
 type Props = {
-  channels: APIChannel[]
+  channels: APIChannel[];
 };
 
+const ChannelSelection: NextPage<Props> = ({ channels }) => {
+  const channelArray = Object.values(channels);
+  const options = [{value: '', label: 'Select a channel'}];
+  channelArray.forEach((c) => options.push({ value: c.id, label: c.name }));
 
-const ChannelSelection: NextPage<Props> = ({channels}) => {
-  const options = [{}]
-  channels.forEach((c) => options.push({value: c.id, label: c.name}))
-  
   return (
     <>
       <Select
@@ -23,7 +21,15 @@ const ChannelSelection: NextPage<Props> = ({channels}) => {
         isSearchable
         isClearable
         options={options}
-        
+        theme={(theme) => ({
+          ...theme,
+          borderRadius: 0,
+          colors: {
+            ...theme.colors,
+            primary25: "hotpink",
+            primary: "black",
+          },
+        })}
       />
     </>
   );
