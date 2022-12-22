71f948b904aa5815fad818ac60ef7598b6692a0c
BossDaily <gboys2008@gmail.com>
2022-12-21 20:52:57 -0600

    fetch post request to discord

--- apps/dashboard/src/pages/api/discord/postEmbed.ts
+++ apps/dashboard/src/pages/api/discord/postEmbed.ts
@@ -1,13 +1,27 @@
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { embed } = req.body
+  const { embed, channel } = req.body
 
   if (!embed) {
     return res.status(400).json({ error: 'Missing embed' })
   }
   
-  
+  const disRes = await fetch(`https://discord.com/api/v10/channels/${channel}/messages`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+      },
+      body: JSON.stringify({
+        embed: embed,
+      }),
+    
+    })
+
+  const disReq = await disRes.json()
+
+  res.status(200).json(disReq);
 }
 
 
