d0bb36fa0349051233b6dc03a08ef915942b26a2
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-02-02 14:55:20 +0000

    updated tailwind config

--- apps/dashboard/tailwind.config.js
+++ apps/dashboard/tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    '../../node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-    '../../node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
-    './src/**/*.{js,jsx,ts,tsx}',
-    './public/**/*.html'
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "../../node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "../../node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/**/*.html",
   ],
   theme: {
     fontFamily: {
-      'header': ['Helvetica', 'Arial', 'sans-serif'],
-    }
+      header: ["Helvetica", "Arial", "sans-serif"],
+    },
   },
-  plugins: [require('flowbite/plugin')],
-  darkMode:"media",
-}
+  plugins: [require("flowbite/plugin")],
+  darkMode: "media",
+};
