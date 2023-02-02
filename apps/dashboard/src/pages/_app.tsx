c5bd43712434f0f25d1d212d1b0c0bfadf7f2290
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-02-02 15:00:05 +0000

    updated import for globals css

--- apps/dashboard/src/pages/_app.tsx
+++ apps/dashboard/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "../../app/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import { Flowbite } from "flowbite-react";
