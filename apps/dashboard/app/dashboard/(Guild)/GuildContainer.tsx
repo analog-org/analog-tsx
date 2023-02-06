d82c8b7b92862753bd0628fff23ee6fb106f9232
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-02-06 15:17:17 +0000

    converted to new component

--- apps/dashboard/app/dashboard/(Guild)/GuildContainer.tsx
+++ apps/dashboard/app/dashboard/(Guild)/GuildContainer.tsx
@@ -1,11 +1,13 @@
+"use client"
+
 import { NextPage } from "next";
 import { ReactNode } from "react";
 
 type Props = {
-  children: ReactNode;
+  children: React.ReactNode;
 };
 
-const GuildContainer: NextPage<Props> = ({ children }) => {
+export default function GuildContainer ({ children }: Props) {
   return (
     <div className=" px-4 py-8 mx-auto sm:px-6 lg:px-8 rounded-xl flex flex-col sm:flex-row flex-initial flex-wrap justify-center">
       <div className="flex flex-col gap-8 rounded-xl sm:grid sm:grid-cols-2 sm:grid-row lg:grid-cols-3">
@@ -14,5 +16,3 @@ const GuildContainer: NextPage<Props> = ({ children }) => {
     </div>
   );
 };
-
-export default GuildContainer;
