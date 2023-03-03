2924a8358d0b821ad8147965b5dec9d5c0589cb7
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-03-03 16:08:34 +0000

    updated to the same sidebar analog uses

--- apps/dashboard/app/dashboard/[guildid]/Sidebar.tsx
+++ apps/dashboard/app/dashboard/[guildid]/Sidebar.tsx
@@ -1,32 +1,37 @@
-"use client"
+"use client";
 
-import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next";
-import { ReactNode } from "react";
-import Image from "next/image";
 import { Avatar, Dropdown, Sidebar } from "flowbite-react";
-import { authOptions } from "../../../src/pages/api/auth/[...nextauth]";
-import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
 
-export default function SideBar() {
+/* interface Props {
+  pages: string[]
+} */
+export default function SideBar({ pages, id }) {
+  const balls = ["embed", "test", "test2", "test3"];
   return (
-    <div className="w-fit">
-      <Sidebar aria-label="Sidebar with logo branding example" >
+    <div className="w-fit rounded-none">
+      <Sidebar aria-label="Sidebar with logo branding example">
         <Sidebar.Logo
           href="https://avatars.githubusercontent.com/u/110413696?s=200&v=4"
           img="https://avatars.githubusercontent.com/u/110413696?s=200&v=4"
           imgAlt="Analog logo"
+          className="rounded-full"
         >
           Analog
         </Sidebar.Logo>
         <Sidebar.Items>
           <Sidebar.ItemGroup>
-            <Sidebar.Item href="#">Dashboard</Sidebar.Item>
-            <Sidebar.Item href="/embed">Embed Builder</Sidebar.Item>
+            <Sidebar.Item href={`../${id}`}>Dashboard</Sidebar.Item>
+            {pages.map((page) => (
+              <Link href={`/dashboard/${id}/${page}`}>
+                <div className="flex text-left  rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-zinc-800">
+                  <p className=" px-3">{page}</p>
+                </div>
+              </Link>
+            ))}
           </Sidebar.ItemGroup>
         </Sidebar.Items>
       </Sidebar>
     </div>
   );
-};
-
-
+}
