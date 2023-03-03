6feedf95cd0c6c917960e4db67b62fd5ee89b9f7
BossDaily <75937850+BossDaily@users.noreply.github.com>
2023-03-03 16:09:32 +0000

    updated to use same layout as analog

--- apps/dashboard/app/dashboard/[guildid]/layout.tsx
+++ apps/dashboard/app/dashboard/[guildid]/layout.tsx
@@ -2,20 +2,85 @@ import MainFooter from "./Footer";
 import NavBar from "../../Navbar";
 import SideBar from "./Sidebar";
 import { ReactNode } from "react";
+import DashNavbar from "./DashNav";
+import path from "node:path";
+import { authOptions } from "../../../src/pages/api/auth/[...nextauth]";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { getServerSession } from "next-auth/next";
+import fs, { read, readdirSync } from "fs";
+import perms from "../../../src/utils/bitfield";
+import { APIGuild } from "discord-api-types/v10";
 
-type LayoutProps = {
-  children: React.ReactNode;
+const filelist = path.join(process.cwd(), "app/dashboard/[guildid]");
+const componentFiles = fs
+  .readdirSync(filelist)
+  .filter((file) => !file.endsWith(".tsx"));
+const components = [];
+for (const file of componentFiles) {
+  components.push(file);
 }
 
-export default function DashboardLayout ({ children }: LayoutProps){
-  return (
-    <div className="bg-gray-800">
-      <NavBar />
-      <div className="flex">
-        <SideBar />
-        <div className="flex-grow">{children}</div>
-      </div>
-      <MainFooter />
-    </div>
+export default async function DashboardLayout({ children, params }) {
+  const { guildid } = params;
+
+  const session = await getServerSession(authOptions);
+
+  const guildFetch = await fetch(
+    `https://discord.com/api/v10/users/@me/guilds`,
+    {
+      headers: {
+        // @ts-ignore
+        Authorization: `Bearer ${session?.accessToken}`,
+      },
+    }
   );
-};
+  const guilds: APIGuild[] = await guildFetch.json();
+
+  const botGuildsFetch = await fetch(
+    `https://discord.com/api/v10/users/@me/guilds`,
+    {
+      headers: {
+        // @ts-ignore
+        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+      },
+    }
+  );
+  const botGuilds: APIGuild[] = await botGuildsFetch.json();
+
+  const userGuilds: APIGuild[] = [];
+
+  if (Array.isArray(guilds)) {
+    guilds.map((gld: APIGuild) => {
+      const serverPerms = perms(gld.permissions);
+      if (serverPerms.includes("MANAGE_GUILD")) {
+        if (botGuilds.some((botGuild: APIGuild) => botGuild.id === gld.id)) {
+          userGuilds.push(gld);
+        }
+      }
+    });
+  }
+
+  if (userGuilds.some((userGld: APIGuild) => userGld.id === guildid)) {
+    return (
+      <div className="bg-zinc-800">
+        <DashNavbar />
+        <div className="flex">
+          <SideBar pages={components} id={guildid} />
+          <div className="flex-grow p-6">{children}</div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="bg-zinc-800">
+        <DashNavbar />
+        <div className="flex">
+          <SideBar pages={components} id={guildid} />
+          <div className="flex-grow p-6">
+            You are not in this guild or you do not have admin access
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
